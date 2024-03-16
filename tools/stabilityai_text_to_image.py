488b0752d187c18cb58ef31c3a8104217bb63ddd
tharuka-pavith <tharukapavith2@gmail.com>
2024-03-16 20:15:41 +0530

    update(#NA): Add firebase storage functionality

--- tools/stabilityai_text_to_image.py
+++ tools/stabilityai_text_to_image.py
@@ -1,35 +1,35 @@
+import datetime
+import io
 import os
 import requests
 from dotenv import load_dotenv
-from langchain.tools import BaseTool, StructuredTool, tool
-from pydantic import BaseModel, Field
-
+from langchain.tools import tool
+from firebase.firebase_config import upload_blob
 
 load_dotenv()
 
 HUGGINGFACEHUB_API_TOKEN = os.environ["HUGGINGFACEHUB_API_TOKEN"]
-
 API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
 headers = {"Authorization": f"Bearer {HUGGINGFACEHUB_API_TOKEN}"}
 
 
-class Input(BaseModel):
-    inputs: str = Field(description="Text prompt that is going to send to the image generator")
-
-
 @tool
-def generate_image(payload):
-    """Use this function to generate image according to the given text prompt"""
+def generate_image(prompt: str) -> str:
+    """Use this function to generate image according to the given text prompt
+    and to get the url of the generated image."""
+    payload = {"inputs": prompt}
     response = requests.post(API_URL, headers=headers, json=payload)
     # return response.content
-    return "dummy.com/gummy_image.jpg"
+    image_bytes = response.content
+    # image_bytes = generate_image(Input(inputs="Astronaut riding a horse"))
+    image = io.BytesIO(image_bytes)
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")  # Format example: 20240304_154625
+    with image as f:
+        url = upload_blob(f, f"{timestamp}.jpg")
+        return str(url)
 
-
-# import io
-# from PIL import Image
-# image = Image.open(io.BytesIO(image_bytes))
 
 if __name__ == "__main__":
-    # image_bytes = generate_image({"inputs": "Astronaut riding a horse", })
-    image_bytes = generate_image(Input(inputs="Astronaut riding a horse"))
-    print("Hi")
+    # image_bytes = generate({"inputs": "A black benz car on the moon", })
+    url = generate_image("A dog sleeping under a tree")
+    print(url)
