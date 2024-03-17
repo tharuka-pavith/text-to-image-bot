26e5c3c1659fe8990a4239fcc74b9390a10e170e
tharuka-pavith <tharukapavith2@gmail.com>
2024-03-17 21:38:59 +0530

    update(#NA): Add service account key

--- firebase/firebase_config.py
+++ firebase/firebase_config.py
@@ -4,10 +4,25 @@ from firebase_admin import storage
 import firebase_admin
 from firebase_admin import credentials
 from dotenv import load_dotenv
-
 load_dotenv()
 
-cred = credentials.Certificate(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
+SERVICE_ACCOUNT_KEY = {
+    "type": os.environ["TYPE"],
+    "project_id": os.environ["PROJECT_ID"],
+    "private_key_id": os.environ["PRIVATE_KEY_ID"],
+    "private_key": os.environ["PRIVATE_KEY"],
+    "client_email": os.environ["CLIENT_EMAIL"],
+    "client_id": os.environ["CLIENT_ID"],
+    "auth_uri": os.environ["AUTH_URI"],
+    "token_uri": os.environ["TOKEN_URI"],
+    "auth_provider_x509_cert_url": os.environ["AUTH_PROVIDER_X509_CERT_URL"],
+    "client_x509_cert_url": os.environ["CLIENT_X509_CERT_URL"],
+    "universe_domain": os.environ["UNIVERSE_DOMAIN"]
+}
+
+
+# cred = credentials.Certificate(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
+cred = credentials.Certificate(SERVICE_ACCOUNT_KEY)
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'text-to-image-bot-657bb.appspot.com'
 })
