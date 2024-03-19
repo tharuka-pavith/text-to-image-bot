18b4e28f74664127f99689fb9287d591cf566af9
tharuka-pavith <tharukapavith2@gmail.com>
2024-03-19 22:53:26 +0530

    update(#NA): Add API description

--- main.py
+++ main.py
@@ -5,10 +5,30 @@ from models.models import HumanMessageModel, AIMessageModel
 from handlers.chat_handler import chat_agent_response
 from dotenv import load_dotenv
 load_dotenv()
-app = FastAPI()
+
+description = """
+GAN Based Text to Image Synthesizer API
+### Description
+
+Generate images according to text descriptions.
+
+"""
+tags_metadata = [
+    {
+        "name": "chat-bot",
+        "description": "Endpoint to chat with the bot",
+    },
+]
+
+app = FastAPI(
+    title="Text-to-Image Bot API (dev)",
+    description=description,
+    version="0.1.0",
+    openapi_tags=tags_metadata
+)
 
 
-@app.post("/chat-bot")
+@app.post("/chat-bot", description="Endpoint to chat with the bot",tags=["chat-bot"])
 def chat_endpoint(chat: HumanMessageModel) -> AIMessageModel:
     response = chat_agent_response(chat)
     return response
